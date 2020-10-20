e880a10384fafb329246d9847dcde3946bcffb6f
Jeff Przylucki <jeffpriz@hotmail.com>
2020-10-20 12:47:50 -0500

    calling API successfully

--- src/contributions/PRInfo/PRInfo.tsx
+++ src/contributions/PRInfo/PRInfo.tsx
@@ -1,32 +1,62 @@
 import * as React from "react";
 import * as SDK from "azure-devops-extension-sdk";
+import * as API from "azure-devops-extension-api";
 import { GitServiceIds, IVersionControlRepositoryService } from "azure-devops-extension-api/Git/GitServices";
 
 import { Header, TitleSize } from "azure-devops-ui/Header";
 import { Page } from "azure-devops-ui/Page";
-
+import { GitRestClient, GitPullRequest, PullRequestStatus, GitPullRequestSearchCriteria, GitBranchStats } from "azure-devops-extension-api/Git";
+import { CommonServiceIds, getClient, IProjectPageService } from "azure-devops-extension-api";
 import { showRootComponent } from "../../Common";
 import { GitRepository } from "azure-devops-extension-api/Git/Git";
 
 interface IRepositoryServiceHubContentState {
     repository: GitRepository | null;
+    exception: string | null;
+    prCount: number;
 }
 
 class RepositoryServiceHubContent extends React.Component<{}, IRepositoryServiceHubContentState> {
     constructor(props: {}) {
         super(props);
         
-        this.state = { repository: null };
+        this.state = { repository: null,   exception: null, prCount:-1 };
     }
 
-    public async componentWillMount() {
-        SDK.init();
+    public async componentDidMount() {
+        await SDK.init();
         const repoSvc = await SDK.getService<IVersionControlRepositoryService>(GitServiceIds.VersionControlRepositoryService);
-        const repository = await repoSvc.getCurrentGitRepository();
-
+        var repository = await repoSvc.getCurrentGitRepository();
+        var exception = "";       
+        var aToken = await SDK.getAccessToken();
+        var url = await SDK.getUser
+        var count:number = -1;
+        try{
+            if(repository)
+            {            
+                let searchCriteria: GitPullRequestSearchCriteria = {status:PullRequestStatus.Completed, includeLinks:true, creatorId: "", reviewerId: "", repositoryId: "", sourceRefName: "",targetRefName:"", sourceRepositoryId: ""};
+                const client = getClient(GitRestClient);
+                let prList: GitPullRequest[] = await client.getPullRequests(repository.id, searchCriteria);
+                count = prList.length;
+            }
+        }
+            catch(ex)
+        {
+            exception = " Error Retrieving Pull Requests -- " + ex.toString();
+        }
+        
         this.setState({
-            repository
+            repository,  exception, prCount:count
         });
+    }
+
+    public async GetGitAPIClient(repositoryID:string)
+    {
+        if(repositoryID)
+        {            
+            let searchCriteria: GitPullRequestSearchCriteria = {status:PullRequestStatus.Completed, includeLinks:true, creatorId:"", reviewerId: "", repositoryId: repositoryID, sourceRefName: "",targetRefName:"", sourceRepositoryId: repositoryID};
+            let prList: GitPullRequest[] = await API.getClient(GitRestClient).getPullRequests(repositoryID, searchCriteria);
+        }
     }
 
     public render(): JSX.Element {
@@ -52,6 +82,16 @@ class RepositoryServiceHubContent extends React.Component<{}, IRepositoryService
                     {
                         this.state.repository &&
                         <p>{this.state.repository.url}</p>
+                    }
+                    <h3>Count</h3>
+                    {
+                        this.state.prCount &&
+                        <p>{this.state.prCount}</p>
+                    }
+                    <h3>Ex</h3>
+                    {
+                        this.state.exception &&
+                        <p>{this.state.exception}</p>
                     }
                 </div>
             </Page>
